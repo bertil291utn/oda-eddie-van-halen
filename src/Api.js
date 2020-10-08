a7d2eb63b50e59318c51589a5e51bc75c4f8fe6d
Bertil Tandayamo <bertilt4@gmail.com>
2020-10-08 16:59:16 -0500

    Add get Spotify token function
    - To get the token everytime the application inits

--- src/Api.js
+++ src/Api.js
@@ -1,10 +1,34 @@
 import SpotifyWebApi from 'spotify-web-api-js';
 
 const API = (() => {
-  const spotifyApi = new SpotifyWebApi();
-  spotifyApi.setAccessToken(process.env.REACT_APP_SPOTIFY_KEY);
+
+
+  const getSpotifyToken = async () => {
+    const myHeaders = new Headers();
+    const basic = `Basic ${btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`)}`;
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+    myHeaders.append('Authorization', basic);
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append('grant_type', 'client_credentials');
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow',
+    };
+
+    const baseURL = 'https://accounts.spotify.com/api/token';
+    const response = await fetch(baseURL, requestOptions);
+
+    return response.json();
+  };
 
   const getArtist = async () => {
+    const spotifyApi = new SpotifyWebApi();
+    const token = await getSpotifyToken();
+    spotifyApi.setAccessToken(token.access_token);
     try {
       const data = await spotifyApi.searchTracks('Love');
       console.log(data);
