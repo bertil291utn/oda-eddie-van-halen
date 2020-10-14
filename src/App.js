a203c0fec6448e1887a61e79a2d2a14b94fbfd77
Bertil Tandayamo <bertilt4@gmail.com>
2020-10-13 23:44:25 -0500

    Add switch router element to app page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import TrackList from './containers/TrackList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import EddiesBio from './components/EddiesBio';
+import BackgroundImage from './assets/images/background.jpg';
+import NavBar from './components/NavBar';
+
 
 function App() {
+  const stylesTrackList = {
+    background: `url(${BackgroundImage})`,
+    height: '100vh',
+
+  };
   return (
-    <div className="App">
-      <TrackList />
+    <div className="App" style={stylesTrackList}>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route path="/albums">
+              <TrackList />
+            </Route>
+            <Route path="/">
+              <EddiesBio />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
